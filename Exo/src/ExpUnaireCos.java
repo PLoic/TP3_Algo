199df3146dcfc573c35b65dd127a6edb4e61e423
Loïc <loic.pauletto@gmail.com>
2016-02-18 15:16:57 +0100

    First Question

---
+++ Exo/src/ExpUnaireCos.java
@@ -0,0 +1,9 @@
+/**
+ * Created by loic on 18/02/2016.
+ */
+public class ExpUnaireCos extends ExpUnaire {
+    @Override
+    String toStringInfixe() {
+        return null;
+    }
+}
---
+++ Exo/src/ExpUnaireSin.java
@@ -0,0 +1,9 @@
+/**
+ * Created by loic on 18/02/2016.
+ */
+public class ExpUnaireSin extends ExpUnaire {
+    @Override
+    String toStringInfixe() {
+        return null;
+    }
+}
